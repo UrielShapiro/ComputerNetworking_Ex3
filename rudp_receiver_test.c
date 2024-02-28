5e2803b58c7e60231d0ae379107acedcd9027a63
Chaim Averbach <chaim.averbach@gmail.com>
2024-02-28 17:24:37 +0200

    Made receiver_test try until error or close

--- rudp_receiver_test.c
+++ rudp_receiver_test.c
@@ -7,22 +7,23 @@ int main(void)
     if (!receiver)
         return 1;
     printf("Created receiver!\n");
-    char buffer[1024];
+    char buffer[1024] = { 0 };
     printf("Attempting to receive message...\n");
-    int recv_result;
+    int recv_result = 0;
+    int recs_left = 10;
     do
     {
         recv_result = rudp_recv(receiver, buffer, sizeof(buffer));
-    } while (recv_result != -2 && recv_result != -1);
+        if (recv_result > 0) printf("Received: \"%s\"\n", buffer);
+    } while (recv_result != -1 && recv_result != -2 && recs_left-->0);
     if (recv_result == -1)
     {
-        printf("Received: \"%s\"\n", buffer);
-        rudp_close_receiver(receiver);
         printf("Sender closed the connection\n");
     }
     else
     {
         printf("Errored out\n");
     }
+    rudp_close_receiver(receiver);
     return 0;
 }
