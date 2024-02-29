c7c4e6b7710134439d19ba3f35e4454ecac29d0b
Chaim Averbach <chaim.averbach@gmail.com>
2024-02-29 11:30:54 +0200

    Made rudp_sender_test loopy

--- rudp_sender_test.c
+++ rudp_sender_test.c
@@ -8,16 +8,23 @@ int main(void)
     if (!sender)
         return 1;
     printf("Created sender!\n");
-    char *message = "Hello through RUDP!";
-    printf("Attempting to send message: \"%s\"...\n", message);
-    if (rudp_send(sender, message, strlen(message) + 1) < 0)
+    char message[1024];
+    printf("Starting sending loop, send \"close\" to finish.\n");
+    do
     {
-        printf("rudp_send failed!\n");
-    }
-    else
-    {
-        printf("Sent message.\n");
-    }
+        printf("Enter message: ");
+        fgets(message, 1024, stdin);
+        message[strcspn(message, "\n")] = '\0';
+        printf("Attempting to send message: \"%s\"...\n", message);
+        if (rudp_send(sender, message, strlen(message) + 1) < 0)
+        {
+            printf("rudp_send failed!\n");
+        }
+        else
+        {
+            printf("Sent message.\n");
+        }
+    } while (strcmp(message, "close") != 0);
     rudp_close_sender(sender);
     printf("Closed correctly\n");
     return 0;
